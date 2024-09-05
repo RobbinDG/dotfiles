909ca56d5a57c5200b771461cfb8f805b615278f
Robbin de Groot <robbindegroot@gmail.com>
2024-09-05 12:56:06 +0200

    Updated system settings to reflect os

--- .bashrc.d/misc.sh
+++ .bashrc.d/misc.sh
@@ -2,8 +2,11 @@ export PATH="$PATH:~/development/flutter/bin"
 export PATH="$PATH:/usr/lib/dart/bin"
 export PATH="$PATH:~/.dotnet/tools"
 
-# export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dawt.useSystemAAFontSettings=on -Dsun.java2d.dpiaware=true'
-export _JAVA_AWT_WM_NONREPARENTING=1
-export GDK_SCALE=2
-export QT_SCALE_FACTOR=2
-# export GDK_DPI_SCALE=2.00
+. /etc/os-release
+if [[ $ID = "arch" ]]; then
+    # export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dawt.useSystemAAFontSettings=on -Dsun.java2d.dpiaware=true'
+    export _JAVA_AWT_WM_NONREPARENTING=1
+    export GDK_SCALE=2
+    export QT_SCALE_FACTOR=2
+    # export GDK_DPI_SCALE=2.00
+fi
